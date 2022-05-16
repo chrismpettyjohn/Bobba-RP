a37d08eac9cb2d8a95c123d3fe6bc116202f6f9c
Chris Pettyjohn <chrismmpettyjohn>
2022-05-16 00:32:33 -0500

    Add icons to sidebar

--- packages/bobba-theme/src/components/sidebar/Sidebar.tsx
+++ packages/bobba-theme/src/components/sidebar/Sidebar.tsx
@@ -1,26 +1,32 @@
 import {Link} from 'wouter';
 import React, {ReactNode, useContext} from 'react';
-import {configContext, Icon} from '@instinct-web/core';
 import {AboutInstinct} from '../about-instinct/AboutInstinct';
+import {configContext, Avatar, sessionContext, Icon} from '@instinct-web/core';
 
 export function Sidebar() {
+  const {user} = useContext(sessionContext);
   const {config} = useContext(configContext);
 
-  const navLinks: Array<{path: string; text: ReactNode}> = [
+  const navLinks: Array<{path: string; text: ReactNode; icon: string}> = [
     {
       path: '/',
+      icon: 'home',
       text: 'Home',
     },
     {
       path: '/community/news',
+      icon: 'users',
       text: 'Community',
     },
     {
       path: '/lifestyle/jobs',
+      icon: 'building',
+
       text: 'Lifestyle',
     },
     {
       path: '/underground/gangs',
+      icon: 'skull',
       text: 'Underground',
     },
   ];
@@ -51,6 +57,16 @@ export function Sidebar() {
               <Link to={link.path} key={`sidebar_link_${link.path}`}>
                 <a className="nav-item" data-v-65a92196="">
                   <span className="nav-label" data-v-65a92196="">
+                    <Icon
+                      type={link.icon}
+                      style={{
+                        color: '#40C8D7',
+                        fontSize: 24,
+                        position: 'absolute',
+                        top: 13,
+                        left: 8,
+                      }}
+                    />
                     {link.text}
                   </span>
                 </a>
