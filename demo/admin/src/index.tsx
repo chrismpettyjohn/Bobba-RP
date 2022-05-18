aad4162ec998ba9244a34ad673c1f2e7f46cc48e
Chris Pettyjohn <chrismmpettyjohn>
2022-05-18 03:42:55 -0500

    oops{

--- demo/admin/src/index.tsx
+++ demo/admin/src/index.tsx
@@ -2,6 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { InstinctWeb } from './InstinctWeb';
 import '@instinct-web/core/dist/frontend.css';
-import '@bobba-rp/theme/dist/public/css/bobba-rp-theme.css';
+import '@instinct-web/admin/dist/admin.css';
 
 ReactDOM.render(<InstinctWeb />, document.getElementById('root'));
