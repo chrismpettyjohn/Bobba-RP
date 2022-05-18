5351cf183bfa9af96ea8ad37c8705fc835159d00
Chris Pettyjohn <chrismmpettyjohn>
2022-05-18 02:16:40 -0500

    prettier

--- demo/web/src/index.tsx
+++ demo/web/src/index.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { InstinctWeb } from './InstinctWeb';
-import '@instinct-web/admin/dist/admin.css';
 import '@instinct-web/core/dist/frontend.css';
 import '@bobba-rp/theme/dist/public/css/bobba-rp-theme.css';
 
