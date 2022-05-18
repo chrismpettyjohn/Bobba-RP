c3c5358e288ff23f0770347d4b955a23dfec52b5
Chris Pettyjohn <chrismmpettyjohn>
2022-05-18 01:20:02 -0500

    Update footer

--- packages/bobba-theme/src/components/about-instinct/AboutInstinct.tsx
+++ packages/bobba-theme/src/components/about-instinct/AboutInstinct.tsx
@@ -2,29 +2,35 @@ import React from 'react';
 
 export function AboutInstinct() {
   return (
-    <div style={{margin: '0 auto'}}>
-      <div
-        style={{
-          color: '#0D7776',
-          fontSize: 35,
-          fontWeight: 'bold',
-          letterSpacing: 1.4,
-          width: '100%',
-        }}
-      >
-        INSTINCT
+    <a
+      href="https://instinct.dev"
+      target="_blank"
+      style={{textDecoration: 'none'}}
+    >
+      <div style={{margin: '0 auto'}}>
+        <div
+          style={{
+            color: '#0D7776',
+            fontSize: 35,
+            fontWeight: 'bold',
+            letterSpacing: 1.4,
+            width: '100%',
+          }}
+        >
+          INSTINCT
+        </div>
+        <div
+          style={{
+            color: 'white',
+            fontSize: 15,
+            fontWeight: 'bold',
+            letterSpacing: 1.4,
+            marginLeft: 60,
+          }}
+        >
+          By LeChris
+        </div>
       </div>
-      <div
-        style={{
-          color: 'white',
-          fontSize: 15,
-          fontWeight: 'bold',
-          letterSpacing: 1.4,
-          marginLeft: 60,
-        }}
-      >
-        By LeChris
-      </div>
-    </div>
+    </a>
   );
 }
