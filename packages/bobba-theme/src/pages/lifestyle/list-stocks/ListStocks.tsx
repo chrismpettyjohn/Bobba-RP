f5982489eebc8ef01f44113958973b04f4e57009
Chris Pettyjohn <chrismmpettyjohn>
2022-05-17 22:18:14 -0500

    Delete unused pages

--- packages/bobba-theme/src/pages/lifestyle/list-stocks/ListStocks.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import {setURL} from '@instinct-web/core';
-import {LifestyleLayout} from '../LifestyleLayout';
-
-setURL('lifestyle/stocks', <ListStocks />);
-
-export function ListStocks() {
-  return <LifestyleLayout results={0}>Stocks</LifestyleLayout>;
-}
