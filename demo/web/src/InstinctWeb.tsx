8152877a85dff1c24d4faa77643d8e397abfe97f
Chris Pettyjohn <chrismmpettyjohn>
2022-05-18 21:22:36 -0500

    add admin panel back

---
+++ demo/web/src/InstinctWeb.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {AdminPanel} from '@instinct-web/admin';
+import {RoleplayTheme} from '@instinct-theme/bobba-rp';
+
+export function InstinctWeb() {
+  return (
+    <>
+      <RoleplayTheme />
+      <AdminPanel />
+    </>
+  );
+}
