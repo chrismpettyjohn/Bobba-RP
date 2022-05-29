f24ccf045aa97718b1cb5a9f17060645d68b2e85
Chris Pettyjohn <chrismmpettyjohn>
2022-05-29 05:29:31 -0500

    Clean up profile header

--- packages/bobba-original-theme/src/pages/home/user-profile/UserProfile.tsx
+++ packages/bobba-original-theme/src/pages/home/user-profile/UserProfile.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useContext} from 'react';
 import './UserProfile.scss';
 import {Badges} from './badges';
 import {useRoute} from 'wouter';
@@ -7,18 +7,19 @@ import {UserContainer} from './user-container';
 import {useFetchRPStatsByUsername} from '@instinct-plugin/bobba-rp-web';
 import {UserLayout} from '../../../components/layout/user/UserLayout';
 import {Container} from '../../../components/generic/container/Container';
-import {Jumbotron} from '../../../components/generic/jumbotron/Jumbotron';
 import {MyEmploymentCard} from '../../../components/templates/my-employment-card';
 import {
-  Column,
+  configContext,
   Loading,
   setURL,
   useFetchUserByUsername,
 } from '@instinct-web/core';
+import {MiniJumbotron} from '../../../components/generic/mini-jumbotron/MiniJumbotron';
 
 setURL('profile/:username', <UserProfile />);
 
 export function UserProfile() {
+  const {config} = useContext(configContext);
   const [match, params] = useRoute<{username: string}>('/profile/:username');
   const profile = useFetchUserByUsername(params!.username);
   const rpStats = useFetchRPStatsByUsername(params!.username);
@@ -26,8 +27,27 @@ export function UserProfile() {
   return (
     <UserLayout section="profile">
       <Loading isLoading={profile === undefined}>
-        <Jumbotron title={`The profile of ${profile?.user?.username}`} />
         <Container>
+          <div className="row">
+            <div className="col-12">
+              <MiniJumbotron>
+                <div className="row">
+                  <div className="col">
+                    <h1>{profile?.user?.username}'s Profile</h1>
+                  </div>
+                  <div className="col text-right">
+                    {profile?.user?.rank?.permissions?.websiteShowStaff && (
+                      <img
+                        src={`${config.rankBadgeURL}/ADM.gif`}
+                        height={55}
+                        width={55}
+                      />
+                    )}
+                  </div>
+                </div>
+              </MiniJumbotron>
+            </div>
+          </div>
           <div className="row">
             <div className="col-4">
               <UserContainer profile={profile} />
