e20e65ca16ed0882c053a43ddee2be69eee3df20
Chris Pettyjohn <chrismmpettyjohn>
2022-06-03 06:15:52 -0500

    add childish gambino easter egg

--- demo/web/src/InstinctWeb.tsx
+++ demo/web/src/InstinctWeb.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import {BobbaOriginalTheme} from '@instinct-theme/bobba-rp';
 import {ContextProviders, Bootstrap} from '@instinct-web/core';
-import {RPUserContextProvider} from '@instinct-plugin/bobba-rp-web/src';
+import {RPUserContextProvider} from '@instinct-plugin/bobba-rp-web';
 
 export function InstinctWeb() {
   return (
