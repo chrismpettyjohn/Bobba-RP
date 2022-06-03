862c01e7ffa7fa22be3a3a9675c47260a5ab571a
Chris Pettyjohn <chrismmpettyjohn>
2022-06-03 17:47:36 -0500

    fix client not showing

--- packages/bobba-theme/src/BobbaTheme.tsx
+++ packages/bobba-theme/src/BobbaTheme.tsx
@@ -3,12 +3,14 @@ import React from 'react';
 import './public/css/RoleplayTheme.scss';
 import {RPUserContextProvider} from '@bobba-rp/web';
 import {ContextProviders, Bootstrap} from '@instinct-web/core';
+import {GameClient} from './components/templates/game-client/GameClient';
 
 export function BobbaTheme() {
   return (
     <ContextProviders>
       <RPUserContextProvider>
         <Bootstrap />
+        <GameClient />
       </RPUserContextProvider>
     </ContextProviders>
   );
