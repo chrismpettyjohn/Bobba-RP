5504ba2e15ddff03671c9265c578895a24178407
Chris Pettyjohn <chrismmpettyjohn>
2022-06-03 08:00:07 -0500

    Add admin panel back

--- demo/web/src/InstinctWeb.tsx
+++ demo/web/src/InstinctWeb.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import {BobbaTheme} from '@bobba-rp/theme';
+import {AdminPanel} from '@instinct-web/admin';
 
 export function InstinctWeb() {
   return (
     <>
       <BobbaTheme />
+      <AdminPanel />
       </>
   )
 }
