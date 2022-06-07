b0099e40ccb65bbbaf03292c72a4d0d0a8a0f6f6
Chris Pettyjohn <chrismmpettyjohn>
2022-06-06 22:57:47 -0500

    Improve exit game button logic a lil.  still pre buggy

--- packages/bobba-theme/src/components/templates/game-client/game-client-actions/exit-game-client-button/ExitGameClientButton.tsx
+++ packages/bobba-theme/src/components/templates/game-client/game-client-actions/exit-game-client-button/ExitGameClientButton.tsx
@@ -1,16 +1,19 @@
+import {useLocation} from 'wouter';
 import React, {useContext} from 'react';
 import {LOG_OUT_DELAY} from '../../renew-sso.hook';
 import {Icon, sessionContext} from '@instinct-web/core';
 import {GameClientRestartTimer} from '../../game-client-restart-timer/GameClientRestartTimer';
 
 export function ExitGameClientButton() {
-  const {setSSO, setUser, setOnline, online} = useContext(sessionContext);
+  const [location, setLocation] = useLocation();
+  const {setSSO, setUser, setOnline} = useContext(sessionContext);
 
   async function onExitClient() {
     return new Promise(resolve => {
       setSSO(undefined);
       setUser({online: false});
       setOnline(false);
+      setLocation('/me');
       setTimeout(() => {
         return resolve(undefined);
       }, LOG_OUT_DELAY);
